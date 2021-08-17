f16c0661c0b08742c64c4fac1bb47731f95ebd58
jyameogo <55655956+oliyam@users.noreply.github.com>
2021-08-17 16:30:02 +0200

    loginpage

--- bin/public/index.js
+++ bin/public/index.js
@@ -10,23 +10,3 @@ function input_validate(x) {
 	else
 		x.style.background = "#ff546e";
 }
-
-var coll = document.getElementsByClassName("collapsible");
-var transition = document.querySelector('.content');
-var cycle = 0;
-
-transition.addEventListener('transitionend', () => {
-	cycle=cycle^1;
-	console.log(cycle);
-});
-
-for (var i = 0; i < coll.length; i++) {
-	coll[i].addEventListener("click", function() {
-		var content = this.nextElementSibling;
-		this.classList.toggle("active");
-		if (content.style.maxHeight)
-			content.style.maxHeight = null;
-		else
-			content.style.maxHeight = content.scrollHeight + "px";
-	});
-}
