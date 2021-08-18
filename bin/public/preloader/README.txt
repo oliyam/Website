858ecc05fb1899ab6dc330bab3644ce330b9f433
jyameogo <55655956+oliyam@users.noreply.github.com>
2021-08-19 00:51:34 +0200

    minor changes

---
+++ bin/public/preloader/README.txt
@@ -0,0 +1,7 @@
+**************************************************************************
+****         WEBSITE LOADING SCREEN - Jan Oliver Yameogo 2021         ****
+**************************************************************************
+
+Copy this folder into the directory of your website's HTML file and paste
+the contents of "preloader.html" directly into the head of your HTML file.
+
--- bin/public/preloader/preloader.js
+++ bin/public/preloader/preloader.js
@@ -1,21 +1,21 @@
 var loader;
-	function loadNow(opacity){
-		if(opacity <= 0){
-			hideLoader();
-		}
-		else{
-			loader.style.opacity = opacity;
-			window.setTimeout(function() { 
-				loadNow(opacity - 0.05);
-			}, 50);
-		}
+function loadNow(opacity){
+	if(opacity <= 0){
+		hideLoader();
 	}
-
-	function hideLoader() {
-		loader.style.display = "none";
+	else{
+		loader.style.opacity = opacity;
+		window.setTimeout(function() { 
+			loadNow(opacity - 0.05);
+		}, 50);
 	}
+}
 
-	window.onload = function() {
-		loader = document.getElementById("preloader");
-		loadNow(1);
-	};
+function hideLoader() {
+	loader.style.display = "none";
+}
+
+window.onload = function() {
+	loader = document.getElementById("preloader");
+	loadNow(1);
+};
