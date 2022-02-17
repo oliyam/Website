1a571580592f223b66b2c402ba79891c45b70012
jyameogo <55655956+oliyam@users.noreply.github.com>
2022-02-17 15:50:28 +0100

    minor changes

--- bin/public/catan/client.js
+++ bin/public/catan/client.js
@@ -6,18 +6,14 @@ const app = new PIXI.Application({
     width: 600, height: 600, backgroundColor: 0x000000, backgroundAlpha: 0.5, antialias: true
 });
 
-var stadt_=false;
-var spieler_=0;
-var position=[];
-var size=50;
 
 app.view.id = "pixijs";
 
-var map=document.getElementById('map');
-map.appendChild(app.view);
-
 var game = new _game();
 var view = new _view(game, app.screen.width, app.screen.height);
+
+var map=document.getElementById('map');
+map.appendChild(app.view);
 
 function redraw(){
     app.stage.removeChild(view);
@@ -39,47 +35,46 @@ map.addEventListener('mouseover', e => {
 
 document.getElementById('bauen').addEventListener('click', e => {
     buildMarkedTiles();
-    view.marked_tiles = [];
+    view.temp.marked_tiles = [];
 });
 
 document.getElementById('loeschen').addEventListener('click', e => {
     game.wege_bauen = new Map();
     game.kreuzungen_bauen = new Map();
-    view.marked_tiles = [];
+    view.temp.marked_tiles = [];
 });
 
 document.getElementById('stadt').addEventListener('click', e => {
-    document.getElementById('stadt').innerText=stadt_?'Siedlung':'Stadt';
-    view.marked_tiles = [];
-    stadt_=!stadt_;
+    document.getElementById('stadt').innerText=stadt?'Siedlung':'Stadt';
+    view.temp.marked_tiles = [];
+    view.temp.stadt=!view.temp.stadt;
     redraw();
 });
 
 document.getElementById('spieler').addEventListener('click', e => {
-    view.marked_tiles = [];
-    spieler_++;
-    spieler_=spieler_%4;
-    document.getElementById('spieler').innerText=spieler_;
+    view.temp.marked_tiles = [];
+    view.temp.spieler=++view.temp.spieler%4;
+    document.getElementById('spieler').innerText=view.temp.spieler;
     redraw()
 });
 
 function buildMarkedTiles(){
-    if(_hex.areNeighbours(view_.marked_tiles))
-    switch(view_.marked_tiles.length){
+    if(_hex.areNeighbours(view.temp.marked_tiles))
+    switch(view.temp.marked_tiles.length){
         case 2:
             if(isFree(marked_tiles)){
-                game.wege_bauen.set(view.marked_tiles, {id: spieler_});
+                game.wege_bauen.set(view.temp.marked_tiles, {id: view.temp.spieler});
                 redraw();
-                view.marked_tiles = [];
+                remp.marked_tiles = [];
             }
             else
             temp_graphics.clear();
             break;
         case 3:
             if(isFree(marked_tiles)){
-                game.kreuzungen_bauen.set(marked_tiles, {id: spieler_, stadt: stadt_});
+                game.kreuzungen_bauen.set(view.temp.marked_tiles, {id: view.temp.spieler, stadt: stadt_});
                 redraw();
-                view.marked_tiles = [];
+                view.temp.marked_tiles = [];
             }
             else
             temp_graphics.clear();
--- bin/public/catan/view.js
+++ bin/public/catan/view.js
@@ -1,7 +1,5 @@
 import * as _hex from "/catan/hex.js";
 
-var size=50;
-
 class graphics extends PIXI.Graphics{
     marked = false;
     q;
@@ -15,14 +13,21 @@ class graphics extends PIXI.Graphics{
 
 export class _view extends PIXI.Container{
 
+    size=50;
     width;
     height;
-    game;
-    positions = new Map();;
     x;
     y;
-    temp_graphics = new PIXI.Graphics();
-    marked_tiles = [];
+    
+    game;
+    positions = new Map();;
+    
+    temp = {
+        temp_graphics: new PIXI.Graphics(),
+        stadt: false,
+        spieler: 0,
+        marked_tiles: []
+    }
 
     constructor(game, width, height){
         super();
@@ -32,7 +37,7 @@ export class _view extends PIXI.Container{
     }    
 
     drawGame(){
-        var hex_x = _hex.getHexSize(size).x, hex_y = _hex.getHexSize(size).y;
+        var hex_x = _hex.getHexSize(this.size).x, hex_y = _hex.getHexSize(this.size).y;
 
         //index der längsten reihe
         var _index = 0, index = 0;
@@ -74,48 +79,48 @@ export class _view extends PIXI.Container{
                     r: data.r
                 };
 
-                var x=this.x+_hex.hexToPixel(hex, size).x;
-                var y=this.y+_hex.hexToPixel(hex, size).y;
+                var x=this.x+_hex.hexToPixel(hex, this.size).x;
+                var y=this.y+_hex.hexToPixel(hex, this.size).y;
 
                 this.positions[hex.q+"/"+hex.r]={x: x, y: y};
 
                 let g = new graphics(hex.q, hex.r);
                 if(!data.blocked){
                     g.beginFill(this.game.farben_landschaften[data.landschaft], 0.6);
-                    g.drawRegularPolygon(x, y, size, 6, 0);
+                    g.drawRegularPolygon(x, y, this.size, 6, 0);
                     if(data.raeuber){
                         g.beginFill(0x000000, 1);  
-                        g.drawCircle(x, y, size/4)
+                        g.drawCircle(x, y, this.size/4)
                     }
                     else if(data.landschaft!="wueste"){
                         g.beginFill(0xF2AC44, 1);
-                        g.drawCircle(x, y, size/4)
+                        g.drawCircle(x, y, this.size/4)
                         let text = new PIXI.Container;
                         text.addChild(new PIXI.Text(data.zahl+(data.zahl==6||data.zahl==9?'.':''), {
                             align: "center",
                             fontFamily: 'Times New Roman',
-                            fontSize: size/3,
+                            fontSize: this.size/3,
                             fontWeight: 'bold',
                             fill: data.zahl==6||data.zahl==8?'red':'black',
                         }));
                         text.x=x-text.width/2;
-                        text.y=y-size/3/2; 
+                        text.y=y-this.size/3/2; 
                         g.addChild(text);
                     }
                     g.endFill();
                     super.addChild(g);
                     let g_outline = new PIXI.Graphics();
                     g_outline.lineStyle(4, 0xF2AC44);
-                    g_outline.drawRegularPolygon(x, y, size, 6, 0);
+                    g_outline.drawRegularPolygon(x, y, this.size, 6, 0);
                     super.addChild(g_outline);
                 }
                 else{
                     g.beginFill(0x2693FF, 0.5);
-                    g.drawRegularPolygon(x, y, size, 6, 0);
+                    g.drawRegularPolygon(x, y, this.size, 6, 0);
                     g.endFill();
                     super.addChild(g);
                 }
-                g.hitArea = new PIXI.Polygon(_hex.getHex(x, y, size, true));
+                g.hitArea = new PIXI.Polygon(_hex.getHex(x, y, this.size, true));
                 g.interactive = true;
                 g.on('pointerover', e => {
                     if(!g.marked)
@@ -127,15 +132,15 @@ export class _view extends PIXI.Container{
                 });
                 g.on('pointerdown', e => {
                     console.log(g.q+" "+g.r)
-                    if(!g.marked&&this.marked_tiles.length<3){
+                    if(!g.marked&&this.temp.marked_tiles.length<3){
                         g.tint = 0x333333;
                         g.marked = true;
-                        this.marked_tiles.push({q: g.q,r: g.r});
+                        this.temp.marked_tiles.push({q: g.q,r: g.r});
                     }
                     else{
                         g.tint = 0x666666;
                         g.marked = false;
-                        this.marked_tiles = this.marked_tiles.filter(tile => !(tile.q==g.q&&tile.r==g.r));
+                        this.temp.marked_tiles = this.temp.marked_tiles.filter(tile => !(tile.q==g.q&&tile.r==g.r));
                     }
                     this.drawMarkedTiles()
                 });
@@ -152,8 +157,8 @@ export class _view extends PIXI.Container{
         graphics.position.y=(this.positions[key[0].q+"/"+key[0].r].y+this.positions[key[1].q+"/"+key[1].r].y)/2;
         graphics.lineStyle(4, 0xF2AC44);
         graphics.beginFill(this.game.farben_spieler[value.id], 1);
-        graphics.drawCircle(-x, -y, size/16);
-        graphics.drawCircle(x, y, size/16);
+        graphics.drawCircle(-x, -y, this.size/16);
+        graphics.drawCircle(x, y, this.size/16);
         graphics.lineStyle(10, 0xF2AC44);
         graphics.moveTo(-x, -y)
         graphics.lineTo(x, y);
@@ -178,7 +183,7 @@ export class _view extends PIXI.Container{
         let graphics = new PIXI.Graphics();
         graphics.lineStyle(4, 0xF2AC44);
         graphics.beginFill(this.game.farben_spieler[value.id], 1);
-        graphics.drawRegularPolygon(pos.x/3, pos.y/3, size*(value.stadt?1/5:1/6), 6, value.stadt*Math.PI/180*30);
+        graphics.drawRegularPolygon(pos.x/3, pos.y/3, this.size*(value.stadt?1/5:1/6), 6, value.stadt*Math.PI/180*30);
         graphics.endFill();
         super.addChild(graphics);
 
@@ -187,13 +192,13 @@ export class _view extends PIXI.Container{
 
     drawMarkedTiles(){
         this.temp_graphics.clear();
-        if(_hex.areNeighbours(this.marked_tiles)&&isFree(this.marked_tiles)){
-            switch(this.marked_tiles.length){
+        if(_hex.areNeighbours(this.temp.marked_tiles)&&isFree(this.temp.marked_tiles)){
+            switch(this.temp.marked_tiles.length){
                 case 2:
-                    this.temp_graphics = this.drawStrasse({id: spieler_}, this.marked_tiles);
+                    this.temp_graphics = this.drawStrasse({id: spieler_}, this.temp.marked_tiles);
                     break;
                 case 3:
-                    this.temp_graphics = this.drawKreuzung({id: spieler_, stadt: stadt_}, this.marked_tiles);
+                    this.temp_graphics = this.drawKreuzung({id: spieler_, stadt: stadt_}, this.temp.marked_tiles);
                     break;
             }
         }
