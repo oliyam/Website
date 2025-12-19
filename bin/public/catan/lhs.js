5b3c84f13d0cfbd45854baba25f672f636637c7e
oliyam <apfel500baum@gmail.com>
2025-12-19 12:20:43 +0100

    update lhs.js

--- bin/public/catan/lhs.js
+++ bin/public/catan/lhs.js
@@ -59,7 +59,7 @@ function longest_paths(st, cl, ck) {
   let checked = ck
   checked.push(st)
   strassen.keys().forEach(pot_nb => {
-    if (!ckecked.includes(pot_nb) && is_nb(pot_nb, st))
+    if (!checked.includes(pot_nb) && is_nb(pot_nb, st))
       longest_paths(pot_nb, 1+current_length, checked)
   });
   lhs.push(current_length)
