3cd80fb4cf997ced11d2253815fa3493e04993c4
oliyam <apfel500baum@gmail.com>
2025-12-19 10:54:49 +0100

    update lhs.js

--- bin/public/catan/lhs.js
+++ bin/public/catan/lhs.js
@@ -1,5 +1,18 @@
 const hex = new (require('../catan/hex.js'))();
 
+const d = [
+  [{q: 1, r: -1},
+   {q: 0, r: 0}],
+   
+  [{q: -1, r: 1},
+   {q: 0, r: 0}],
+   
+  [{q: 0, r: 0},
+   {q: 1, r: -1}],
+   
+  [{q: 0, r: 0},
+   {q: -1, r: 1}],
+]
 const strassen = new Map([
       [[
         {q:3,r:1},
@@ -40,15 +53,22 @@ function get_lhs(){
 
 var lhs = []
 
-function longest_path(st, cl, ck) {
-  let current_longest = cl
+function longest_paths(st, cl, ck) {
+  let current_length = cl
   let checked = ck
   checked.push(st)
-  for(n in hex.areNeighbours(st)){
-    if (checked.indexOf(n)==-1)
-      longest_path(a, ++current_longest, checked)
-  }
-  lhs.push(current_longest)
+  strassen.keys().forEach(pot_nb => {
+    if (is_nb(pot_nb))
+      longest_paths(next, ++current_length, checked)
+  });
+  lhs.push(current_length)
+}
+
+function is_nb(s0, s1) {
+  for(var tile=0;tile<2;tile++)
+    if (s0.indexOf(s1[tile])!=-1)
+      return hex.areNeighbours(s0.concat(s1[(tile+1)%2]))
+  return false;
 }
 
 get_lhs();
